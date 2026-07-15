6bdd54a45f079f85a90a1fe4bebc6b59066cbdd2
Diego Goitia <diegogoitiazx1@gmail.com>
2026-07-14 23:04:11 -0600

    feat: clase myapp para el manejo del estado global de la app

---
+++ MyApp.cs
@@ -0,0 +1,16 @@
+namespace ControlAcceso
+{
+    public class MyApp
+    {
+        // Instancia global de la base de datos
+        public Database Db { get; } = new Database();
+
+        // Estado global: lista de empleados cargada en memoria
+        public List<Empleado> Empleados { get; set; } = new List<Empleado>();
+
+        public void CargarEmpleadosDesdeDb()
+        {
+            Empleados = Db.ObtenerEmpleados();
+        }
+    }
+}
