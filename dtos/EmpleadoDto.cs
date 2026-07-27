1a1358002788855fb9c41dbe77c5b71dac4e1f2b
Diego Goitia <diegogoitiazx1@gmail.com>
2026-07-26 23:29:03 -0400

    fix: se agegaron los campos necesarios para registrar

--- dtos/EmpleadoDto.cs
+++ dtos/EmpleadoDto.cs
@@ -5,7 +5,7 @@ namespace ControlAcceso.DTOs
         public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public int Cedula { get; set; }
-        public byte[]? HuellaBytes { get; set; }
+        //public byte[]? HuellaBytes { get; set; }
         public bool Activo { get; set; }
     }
 }
